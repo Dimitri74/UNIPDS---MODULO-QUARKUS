ef064dd27f97e147fa66415c64209e57d2129303
Marcus Dimitri <marcusdimitri7@gmail.com>
2026-03-03 20:38:14 -0300

    PERSISTÊNCIA DE DADOS COM PANACHE E H2

---
+++ code-with-quarkus/src/main/java/org/unipds/entity/Pessoa.java
@@ -0,0 +1,15 @@
+package org.unipds.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import java.util.List;
+
+@Entity
+public class Pessoa extends PanacheEntity {
+    public String nome;
+    public int anoNascimento;
+
+    public static List<Pessoa> findByAnoNascimento(int anoNascimento) {
+        return find("anoNascimento", anoNascimento).list();
+    }
+}
---
+++ code-with-quarkus/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES(1, 'Dimitri', 1974);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES(2, 'Simone', 1987);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES(3, 'Gabriel', 2005);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES(4, 'Mateus', 2010);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES(5, 'Oliver', 2018);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES(6, 'Marvin', 2018);
+
+ALTER SEQUENCE pessoa_seq RESTART WITH 7;
